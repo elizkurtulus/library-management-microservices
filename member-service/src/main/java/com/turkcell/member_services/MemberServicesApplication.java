d7378c04ab374d2336881a25bd125e80257fcbb5
Ferdanur <ferdanurarmutlu@gmail.com>
2025-11-18 22:11:29 +0300

    Enable service discovery in MemberServicesApplication and expose management endpoints in application.yml

--- member-service/src/main/java/com/turkcell/member_services/MemberServicesApplication.java
+++ member-service/src/main/java/com/turkcell/member_services/MemberServicesApplication.java
@@ -2,8 +2,10 @@ package com.turkcell.member_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MemberServicesApplication {
 
 	public static void main(String[] args) {
